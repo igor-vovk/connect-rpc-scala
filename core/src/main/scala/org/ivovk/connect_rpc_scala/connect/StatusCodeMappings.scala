4dffbd57e8f80f0902bfed6b292e9f2ce263d116
Ihor Vovk <adwolf15@gmail.com>
2025-02-13 22:40:38 +0100

    Add comments

--- core/src/main/scala/org/ivovk/connect_rpc_scala/connect/StatusCodeMappings.scala
+++ core/src/main/scala/org/ivovk/connect_rpc_scala/connect/StatusCodeMappings.scala
@@ -10,23 +10,23 @@ object StatusCodeMappings {
 
     Status.Code.values().foreach { code =>
       codes(code.value()) = code match {
-        case Status.Code.CANCELLED           => 499
-        case Status.Code.UNKNOWN             => 500
-        case Status.Code.INVALID_ARGUMENT    => 400
-        case Status.Code.DEADLINE_EXCEEDED   => 504
-        case Status.Code.NOT_FOUND           => 404
-        case Status.Code.ALREADY_EXISTS      => 409
-        case Status.Code.PERMISSION_DENIED   => 403
-        case Status.Code.RESOURCE_EXHAUSTED  => 429
-        case Status.Code.FAILED_PRECONDITION => 400
-        case Status.Code.ABORTED             => 409
-        case Status.Code.OUT_OF_RANGE        => 400
-        case Status.Code.UNIMPLEMENTED       => 501
-        case Status.Code.INTERNAL            => 500
-        case Status.Code.UNAVAILABLE         => 503
-        case Status.Code.DATA_LOSS           => 500
-        case Status.Code.UNAUTHENTICATED     => 401
-        case _                               => 500
+        case Status.Code.CANCELLED           => 499 // 499 Client Closed Request
+        case Status.Code.UNKNOWN             => 500 // 500 Internal Server Error
+        case Status.Code.INVALID_ARGUMENT    => 400 // 400 Bad Request
+        case Status.Code.DEADLINE_EXCEEDED   => 504 // 504 Gateway Timeout
+        case Status.Code.NOT_FOUND           => 404 // 404 Not Found
+        case Status.Code.ALREADY_EXISTS      => 409 // 409 Conflict
+        case Status.Code.PERMISSION_DENIED   => 403 // 403 Forbidden
+        case Status.Code.RESOURCE_EXHAUSTED  => 429 // 429 Too Many Requests
+        case Status.Code.FAILED_PRECONDITION => 400 // 400 Bad Request
+        case Status.Code.ABORTED             => 409 // 409 Conflict
+        case Status.Code.OUT_OF_RANGE        => 400 // 400 Bad Request
+        case Status.Code.UNIMPLEMENTED       => 501 // 501 Not Implemented
+        case Status.Code.INTERNAL            => 500 // 500 Internal Server Error
+        case Status.Code.UNAVAILABLE         => 503 // 503 Service Unavailable
+        case Status.Code.DATA_LOSS           => 500 // 500 Internal Server Error
+        case Status.Code.UNAUTHENTICATED     => 401 // 401 Unauthorized
+        case _                               => 500 // 500 Internal Server Error
       }
     }
 
